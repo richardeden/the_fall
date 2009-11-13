82a8261798a81f5277e2c88a06e483b0d2a52d61
Tristan Harris & Richard Patching <dev@edendevelopment.co.uk>
2009-11-13 12:15:35 +0000

    DRY up commands in the game controller.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -10,30 +10,38 @@ class GameController < ApplicationController
   
   def leave
     current_player.update_attributes(:active => false)
-    clear_player
+    send_cmd clear_player
     redirect_to root_path
   end    
   
   def board
     @map = Map.load('map1')
-    draw_player
+    send_cmd draw_player
   end
   
   def move
-    clear_player
-    current_player.move(params[:direction])
-    draw_player
+    send_cmd move_player params[:direction]
     render :nothing => true
   end
   
   private
   
-  def move_player
-    Juggernaut.send_to_all "draw_player(#{current_player.data})"
+  def move_player(direction)
+    cmds = clear_player
+    current_player.move(direction)
+    cmds << draw_player
+  end
+  
+  def draw_player
+    "draw_player(#{current_player.data});"
   end
   
   def clear_player
-    Juggernaut.send_to_all "clear_tile(#{current_player.x}, #{current_player.y})"
+    "clear_tile(#{current_player.x}, #{current_player.y});"
+  end
+
+  def send_cmd(cmd)
+    Juggernaut.send_to_all cmd
   end
   
 end
