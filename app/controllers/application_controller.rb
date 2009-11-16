73affa932bcaf0ddcce9f594c8614e7bbe8fc7e9
Richard Patching <richard@edendevelopment.co.uk>
2009-11-16 14:37:13 +0000

    Add current_user helper method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,20 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
-
+  helper_method :current_player
+  helper_method :current_user
   def current_player
     Player.find(session[:player_id])
   end
-  helper_method :current_player
+  
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+  
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
+
 end
