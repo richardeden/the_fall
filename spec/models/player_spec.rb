1d44a38cb332dadd3e15924622c0c45a61d35723
Richard Patching <richard@edendevelopment.co.uk>
2009-11-16 22:51:01 +0000

    Spec for find_or_create player.

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,6 +1,24 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Player do
+  
+  context "find or create" do
+    before(:each) do
+      @player = Player.new
+    end
+    
+    it "returns an existing player if one is found" do
+      Player.should_receive(:find_by_name).with('JoeBloggs').and_return(@player)
+      Player.find_or_create('JoeBloggs').should == @player
+    end
+    
+    it "creates a new player if it can't find an existing player" do
+      Player.stub!(:find_by_name => nil)
+      Player.should_receive(:create).with({:health=>100, :y=>2, :strength=>10, :x=>2, :name=>"JoeBloggs"}).and_return(@player)
+      Player.find_or_create('JoeBloggs').should == @player
+    end
+  end
+  
   context "dead" do
     before(:each) do
       @player = Player.new
