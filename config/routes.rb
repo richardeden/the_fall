2db85e12ed53407f44dc2b18f8e0468f12a63d83
Richard Patching <richard@edendevelopment.co.uk>
2009-11-16 14:40:30 +0000

    Routes for user authentication system.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
 
-  map.root :controller => 'game', :action => 'login'
+  map.root :controller => 'game', :action => 'index'
+  map.resources :users
+  map.resources :user_sessions
+  map.login "login", :controller => 'user_sessions', :action => 'new'
+  map.logout "logout", :controller => 'user_sessions', :action => 'destroy'
+  
+  
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
