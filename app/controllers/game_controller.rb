0f2f265884c1135036622749d489cc66293ae021
Tristan Harris <tris.dev@pumpcottage.me.uk>
2009-11-17 22:51:50 +0000

    Remove functionality from rails controller which is now in the gameserver

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -25,22 +25,6 @@ class GameController < ApplicationController
   def board
   end
 
-  def join
-    cmds = Api.map(Map.load('map1'))
-    Player.active.each do |p|
-      cmds += Api.draw_player(p)
-    end
-    send_cmd Api.new_player(current_player)
-    render :json => cmds
-  end
-  
-  def move
-    render :nothing => true if current_player.dead?
-    cmd = Api.move_player(current_player, params[:direction])
-    send_cmd cmd
-    render :json => cmd
-  end
-
   def create_character
     if request.post?
       if current_player.update_attributes(:avatar => params['avatar']) 
@@ -52,22 +36,4 @@ class GameController < ApplicationController
     end
   end
 
-  def attack
-    victim = current_player.attack(params[:direction])
-    unless victim.nil?
-      if victim.dead?
-        send_cmd Api.player_dead(victim, current_player)
-      else
-        send_cmd Api.attack(victim, current_player)
-      end
-    end
-    render :nothing => true
-  end
-  
-  private
-  
-  def send_cmd(cmds)
-    Juggernaut.send_to_all cmds.to_json
-  end
-  
 end
