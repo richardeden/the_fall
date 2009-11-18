93f35bd37098c1adf3ee7d3df637d1925e88ef1d
Tristan Harris <tris.dev@pumpcottage.me.uk>
2009-11-18 20:31:27 +0000

    Remove redundant controller actions

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -19,42 +19,4 @@ class GameController < ApplicationController
   def board
   end
 
-  def join
-    cmds = Api.map(Map.load('map1'))
-    Player.active.each do |p|
-      cmds += Api.draw_player(p)
-    end
-    send_cmd Api.new_player(current_player)
-    render :json => cmds
-  end
-  
-  def move
-    render :nothing => true if current_player.dead?
-    cmd = Api.move_player(current_player, params[:direction])
-    send_cmd cmd
-    render :json => cmd
-  end
-
-  def attack
-    attack_location = current_player.pre_move(params[:direction])
-    victim = Player.at(*attack_location)
-    unless victim.nil?
-      victim.health -= current_player.strength
-      if victim.dead?
-        victim.active = false
-        send_cmd Api.player_dead(victim, current_player)
-      else
-        send_cmd Api.attack(victim, current_player)
-      end
-      victim.save
-    end
-    render :nothing => true
-  end
-  
-  private
-  
-  def send_cmd(cmds)
-    Juggernaut.send_to_all cmds.to_json
-  end
-  
 end
