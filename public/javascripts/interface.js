2a7e05107c07a191744dae823a1304abc89315fd
Richard Patching <richard@edendevelopment.co.uk>
2009-11-24 14:14:06 +0000

    Move javascript login interface into a seperate javascript file.

---
+++ public/javascripts/interface.js
@@ -0,0 +1,19 @@
+function load_login_interface() {
+  $("#login").dialog({
+    bgiframe: true, autoOpen: false, height: 290, modal: true,
+    buttons:{
+      Login: function() {
+        login();
+      }
+    }
+  });
+
+  $("#pick_player").dialog({
+    bgiframe: true, autoOpen: false, height: 200, modal: true,
+    buttons:{
+      Select: function() {
+        join();
+      }
+    }
+  });
+}
